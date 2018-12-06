bcbec04c8e00fd4c4e9e7e45a8540d92b14ddd3e
metal0-1 <44144806+metal0-1@users.noreply.github.com>
2018-12-06 21:01:32 +0300

    Update Task02.cpp

--- 25.11.18/Task02.cpp
+++ 25.11.18/Task02.cpp
@@ -1,1 +1,50 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+int main() {
+int n;
+cin >> n;
+string hl;
+int h=1;
+if (n % 2 == 0) {
+h = pow(10, n / 2 - 1);
+hl = to_string(h);
+cout << h;
+for (int i =hl.length()- 1; i >= 0; i--) {
+cout << hl[i];
+}
+cout << endl;
+while (h < pow(10, n / 2)-1) {
+h++;
+hl = to_string(h);
+cout << h;
+for (int i = hl.length() - 1; i >= 0; i--) {
+cout << hl[i];
+}
+cout << endl;
+}
+}
+else {
+h = pow(10, (n / 2));
+hl = to_string(h);
+cout << h;
+for (int i = hl.length()-2; i >= 0; i--) {
+cout << hl[i];
+}
+cout << endl;
+while (h < pow(10, (n / 2)+1)-1) {
+h++;
+hl = to_string(h);
+cout << h;
+for (int i = hl.length()-2; i >=0; i--) {
+cout << hl[i];
+}
+cout << endl;
+}
+}
+system("pause");
+return 0;
+}2
+//алгоритм эффективен так как мы рассматриваем только первую часть числа и просто крпируем её соответсвенно количество действий равняется количеству ответов
 

d163c7e0697ed8ebe6f4ada4043ea06341de532a
metal0-1 <44144806+metal0-1@users.noreply.github.com>
2018-12-06 21:00:46 +0300

    Update Task01.cpp

--- 25.11.18/Task01.cpp
+++ 25.11.18/Task01.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+for (int k = 38875; k < 38894; k++) {
+int x; int y;
+x = ((2019 * k) - (78487894));
+y = ((78487894) - (2018 * k));
+cout « "x =" « x « " " « "y =" « y « endl;
+}
+system("pause");
+return 0;
+}
 
